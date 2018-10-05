af0eb1b8b2074d98c21c77b6d92849cbe1189e0c
Jason W <minecrafter10k@gmail.com>
2018-10-04 21:48:32 -0400

    initial bot code

---
+++ Sprint1/Sprint1.ino
@@ -0,0 +1,18 @@
+const int kPinReedSwitch = 3;
+const int kPinServo1 = 9;
+const into kPinServo2 = 10;
+
+void setup() {
+    pinMode(kPinReedSwitch, INPUT);
+    digitalWrite(kPinReedSwitch, HIGH)
+    servoRight.attach(kPinServo1);
+    servoLeft.attach(kPinServo2);
+}
+
+void loop() {
+
+    if(digitalRead(kPinReedSwitch == LOW){
+
+    }
+
+}
